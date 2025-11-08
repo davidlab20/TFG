82ffb378a770ad68f642b4c3fbce41238184f4bf
davidlab <d.diazg.2022@alumnos.urjc.es>
2025-11-08 19:51:21 +0100

    Added default values file

---
+++ aframexr/utils/defaults.py
@@ -0,0 +1,9 @@
+"""Default values utils file"""
+
+# General
+DEFAULT_CHART_POS = '0 0 0'  # Default position of the chart
+
+# Bar chart
+DEFAULT_BAR_CHART_WIDTH = 10  # Default bar chart total width
+DEFAULT_BAR_CHART_HEIGHT = 10  # Default bar chart maximum height
+DEFAULT_BAR_CHART_DEPTH = 1  # Default bar chart depth
