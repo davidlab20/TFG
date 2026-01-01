34fe536034c20e5e5b3efd322638c971d5ba0580
davidlab <d.diazg.2022@alumnos.urjc.es>
2026-01-01 09:05:13 +0100

    Reduced labels' text size

--- aframexr/utils/axis_creator.py
+++ aframexr/utils/axis_creator.py
@@ -106,7 +106,7 @@ class AxisCreator:
             The alignment of the label. The default is 'left'.
         """
 
-        return f'<a-text position="{pos}" rotation="{rotation}" value="{value}" scale="3 3 3" align="{align}"></a-text>'
+        return f'<a-text position="{pos}" rotation="{rotation}" value="{value}" align="{align}"></a-text>'
 
     @staticmethod
     def create_axis_specs(axis: Literal['x', 'y', 'z'], axis_data: Series, axis_encoding: str, axis_size: float,
