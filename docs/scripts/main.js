8822538ef12fedd088d705726a4539081c156755
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-01-27 20:36:07 +0100

    Updated HUD position and rotation, now rotates to the camera

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,21 +1,51 @@
 // Wait for the scene content to load completely
 document.addEventListener('DOMContentLoaded', () => {
 	// Frequently accessed elements
-	const HUD = document.getElementById('HUD')
-	const HUDText = document.getElementById('HUD-text')
+	const HUD = document.getElementById('HUD');
+	const HUDText = document.getElementById('HUD-text');
 
 	// Display information about the element
 	function displayInfo(event) {
 		const targetElement = event.target;
+        const camera = document.getElementById('camera');
+        targetElement.object3D.updateMatrixWorld();
+        camera.object3D.updateMatrixWorld();
 
-	    event.target.setAttribute('scale', '1.1 1.1 1.1');  // Size the scale up
+        const position = new THREE.Vector3();
+        targetElement.object3D.getWorldPosition(position);
 
-        const value = event.target.getAttribute('id')
+		let y_offset = 0;
+        let z_offset = 0;
+        if (targetElement.tagName.toLowerCase() === 'a-box') {  // Bars chart
+            let height = parseFloat(targetElement.getAttribute('height'));
+            const depth = parseFloat(targetElement.getAttribute('depth'));
+            y_offset = height / 2;
+            z_offset = depth / 2;
+        } else if (targetElement.tagName.toLowerCase() === 'a-sphere') {  // Point chart
+            let radius = parseFloat(targetElement.getAttribute('radius'));
+            y_offset = radius;
+            z_offset = radius;
+        } else if (targetElement.tagName.toLowerCase() === 'a-cylinder') {  // Pie chart
+            let radius = parseFloat(targetElement.getAttribute('radius'));
+            y_offset = radius;
+            z_offset = 0;
+        }
 
 		// Update HUD attributes
-        HUD.setAttribute('visible', 'true')
+		HUD.setAttribute('position', {
+            x: position.x,
+            y: position.y + y_offset + 1,
+            z: position.z + z_offset + 0.1
+        });
+        HUD.setAttribute('visible', 'true');
+        const cameraPos = new THREE.Vector3();
+        camera.object3D.getWorldPosition(cameraPos);
+        HUD.object3D.lookAt(cameraPos);
+
+	    targetElement.setAttribute('scale', '1.1 1.1 1.1');  // Size the scale up
 
 	    // Update HUD text attributes
+        const value = targetElement.getAttribute('id');
         HUDText.setAttribute('value', value);
 	}
 
