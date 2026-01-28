f7e040f09ee3ad111074ecf4f626f0fda6ba2a4b
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-01-28 11:28:32 +0100

    New version v0.6.4 of main.js; fixed HUD bug

--- aframexr/utils/scene_creator.py
+++ aframexr/utils/scene_creator.py
@@ -6,7 +6,7 @@ HTML_SCENE_TEMPLATE = """<!DOCTYPE html>
 <head>
     <script src="https://aframe.io/releases/1.7.1/aframe.min.js"></script>
     <script src="https://unpkg.com/aframe-environment-component@1.5.0/dist/aframe-environment-component.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/davidlab20/TFG@v0.6.3/docs/scripts/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/davidlab20/TFG@v0.6.4/docs/scripts/main.min.js"></script>
 </head>
 <body>
     <a-scene cursor="rayOrigin: mouse" raycaster="objects: [data-raycastable]" webxr="requiredFeatures: local-floor">
