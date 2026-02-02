095a3aad124f912ad86973c2ceb10308dcd772ae
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-02-02 13:51:08 +0100

    Updated tests to new syntaxis

--- tests/test_mark_gltf.py
+++ tests/test_mark_gltf.py
@@ -3,15 +3,15 @@ import unittest
 
 from tests.constants import *
 
-DATA = aframexr.URLData('https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/refs/heads/main/Models/'
-                        'AntiqueCamera/glTF/AntiqueCamera.gltf')
+URL = ('https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/refs/heads/main/Models/AntiqueCamera/'
+        'glTF/AntiqueCamera.gltf')
 
 
 class TestMarkGLTFOK(unittest.TestCase):
     """Mark GLTF OK tests."""
     def test_simple(self):
         """Simple GLTF creation."""
-        aframexr.Chart(DATA).mark_gltf().to_html()
+        aframexr.GLTF(URL).to_html()
 
     def test_simple_with_pandas_not_installed(self):
         """Simple GLTF creation without having pandas installed."""
@@ -26,7 +26,7 @@ class TestMarkGLTFOK(unittest.TestCase):
             import aframexr
             importlib.reload(aframexr)
 
-            aframexr.Chart(DATA).mark_gltf().to_html()
+            aframexr.GLTF(URL).to_html()
 
             import aframexr.api.components as components
             self.assertIsNone(components.pd)
@@ -34,7 +34,7 @@ class TestMarkGLTFOK(unittest.TestCase):
 
     def test_from_json(self):
         """GLTF using from_json() method creation."""
-        json_string = aframexr.Chart(DATA).mark_gltf().to_json()
+        json_string = aframexr.GLTF(URL).to_json()
         gltf_chart = aframexr.Chart.from_json(json_string)
         gltf_chart.to_html()
 
@@ -43,64 +43,37 @@ class TestMarkGLTFOK(unittest.TestCase):
     def test_position(self):
         """GLTF changing position creation."""
         for p in POSITIONS:
-            aframexr.Chart(DATA, position=p).mark_gltf().to_html()
+            aframexr.GLTF(URL, position=p).to_html()
 
     def test_position_format(self):
         """GLTF changing position format creation."""
         for p in POSITION_FORMATS:
-            aframexr.Chart(DATA, position=p).mark_gltf().to_html()
+            aframexr.GLTF(URL, position=p).to_html()
 
     def test_rotation(self):
         """GLTF changing rotation creation."""
         for r in ROTATIONS:
-            aframexr.Chart(DATA, rotation=r).mark_gltf().to_html()
+            aframexr.GLTF(URL, rotation=r).to_html()
 
     def test_rotation_format(self):
         """GLTF changing rotation format creation."""
         for r in ROTATION_FORMATS:
-            aframexr.Chart(DATA, rotation=r).mark_gltf().to_html()
+            aframexr.GLTF(URL, rotation=r).to_html()
 
     def test_scale(self):
         """GLTF changing scale creation."""
         for s in MARK_GLTF_SCALES:
-            aframexr.Chart(DATA).mark_gltf(scale=s).to_html()
+            aframexr.GLTF(URL, scale=s).to_html()
 
     def test_position_rotation_scale(self):
         """GLTF changing position, rotation and scale creation."""
         for p, r, s in zip(POSITIONS, ROTATIONS, MARK_GLTF_SCALES):
-            aframexr.Chart(DATA, position=p, rotation=r).mark_gltf(scale=s).to_html()
+            aframexr.GLTF(URL, position=p, rotation=r, scale=s).to_html()
 
     def test_concatenation(self):
         """GLTF concatenation creation."""
-        concatenated_chart = aframexr.Chart(DATA, position=CONCATENATION_POSITIONS[0]).mark_gltf()
+        concatenated_chart = aframexr.GLTF(URL, position=CONCATENATION_POSITIONS[0])
         for pos in CONCATENATION_POSITIONS[1:]:
-            concatenated_chart += aframexr.Chart(DATA, position=pos).mark_gltf()
+            concatenated_chart += aframexr.GLTF(URL, position=pos)
 
         concatenated_chart.to_html()
-
-
-class TestMarkGLTFError(unittest.TestCase):
-    """Mark GLTF error tests."""
-    def test_position_error(self):
-        """Mark GLTF position error."""
-        for p in NOT_3AXIS_POSITIONS_ROTATIONS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA, position=p).mark_gltf()
-            self.assertEqual(str(error.exception), f'The position: {p} is not correct. Must be "x y z".')
-
-        for p in NOT_NUMERIC_POSITIONS_ROTATIONS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA, position=p).mark_gltf()
-            self.assertEqual(str(error.exception), 'The position values must be numeric.')
-
-    def test_rotation_error(self):
-        """Mark GLTF rotation error."""
-        for r in NOT_3AXIS_POSITIONS_ROTATIONS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA, rotation=r).mark_gltf()
-            self.assertEqual(str(error.exception), f'The rotation: {r} is not correct. Must be "x y z".')
-
-        for r in NOT_NUMERIC_POSITIONS_ROTATIONS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA, rotation=r).mark_gltf()
-            self.assertEqual(str(error.exception), 'The rotation values must be numeric.')
--- tests/test_mark_image.py
+++ tests/test_mark_image.py
@@ -3,14 +3,14 @@ import unittest
 
 from tests.constants import *
 
-DATA = aframexr.URLData('https://davidlab20.github.io/TFG/imgs/logo.png')
+URL = 'https://davidlab20.github.io/TFG/imgs/logo.png'
 
 
 class TestMarkImageOK(unittest.TestCase):
     """Mark image OK tests."""
     def test_simple(self):
         """Simple image creation."""
-        aframexr.Chart(DATA).mark_image().to_html()
+        aframexr.Image(URL).to_html()
 
     def test_simple_with_pandas_not_installed(self):
         """Simple image creation without having pandas installed."""
@@ -25,7 +25,7 @@ class TestMarkImageOK(unittest.TestCase):
             import aframexr
             importlib.reload(aframexr)
 
-            aframexr.Chart(DATA).mark_image().to_html()
+            aframexr.Image(URL).to_html()
 
             import aframexr.api.components as components
             self.assertIsNone(components.pd)
@@ -33,7 +33,7 @@ class TestMarkImageOK(unittest.TestCase):
 
     def test_from_json(self):
         """Image using from_json() method creation."""
-        json_string = aframexr.Chart(DATA).mark_image().to_json()
+        json_string = aframexr.Image(URL).to_json()
         image_chart = aframexr.Chart.from_json(json_string)
         image_chart.to_html()
 
@@ -42,83 +42,42 @@ class TestMarkImageOK(unittest.TestCase):
     def test_position(self):
         """Image changing position creation."""
         for p in POSITIONS:
-            aframexr.Chart(DATA, position=p).mark_image().to_html()
+            aframexr.Image(URL, position=p).to_html()
 
     def test_position_format(self):
         """Image changing position format creation."""
         for p in POSITION_FORMATS:
-            aframexr.Chart(DATA, position=p).mark_image().to_html()
+            aframexr.Image(URL, position=p).to_html()
 
     def test_rotation(self):
         """Image changing rotation creation."""
         for r in ROTATIONS:
-            aframexr.Chart(DATA, rotation=r).mark_image().to_html()
+            aframexr.Image(URL, rotation=r).to_html()
 
     def test_rotation_format(self):
         """Image changing rotation format creation."""
         for r in ROTATION_FORMATS:
-            aframexr.Chart(DATA, rotation=r).mark_image().to_html()
+            aframexr.Image(URL, rotation=r).to_html()
 
     def test_width(self):
         """Image changing width creation."""
         for w in MARK_IMAGE_WIDTHS_HEIGHTS:
-            aframexr.Chart(DATA).mark_image(width=w).to_html()
+            aframexr.Image(URL, width=w).to_html()
 
     def test_height(self):
         """Image changing height creation."""
         for h in MARK_IMAGE_WIDTHS_HEIGHTS:
-            aframexr.Chart(DATA).mark_image(height=h).to_html()
+            aframexr.Image(URL, height=h).to_html()
 
     def test_position_rotation_width_height(self):
         """Image changing position, rotation, width and height creation."""
         for p, r, w, h in zip(POSITIONS, ROTATIONS, MARK_IMAGE_WIDTHS_HEIGHTS, MARK_IMAGE_WIDTHS_HEIGHTS):
-            aframexr.Chart(DATA, position=p, rotation=r).mark_image(width=w, height=h).to_html()
+            aframexr.Image(URL, position=p, rotation=r, width=w, height=h).to_html()
 
     def test_concatenation(self):
         """Image concatenation creation."""
-        concatenated_chart = aframexr.Chart(DATA, position=CONCATENATION_POSITIONS[0]).mark_image()
+        concatenated_chart = aframexr.Image(URL, position=CONCATENATION_POSITIONS[0])
         for pos in CONCATENATION_POSITIONS[1:]:
-            concatenated_chart += aframexr.Chart(DATA, position=pos).mark_image()
+            concatenated_chart += aframexr.Image(URL, position=pos)
 
         concatenated_chart.to_html()
-
-
-class TestMarkImageError(unittest.TestCase):
-    """Mark image error tests."""
-    def test_position_error(self):
-        """Mark image position error."""
-        for p in NOT_3AXIS_POSITIONS_ROTATIONS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA, position=p).mark_image()
-            self.assertEqual(str(error.exception), f'The position: {p} is not correct. Must be "x y z".')
-
-        for p in NOT_NUMERIC_POSITIONS_ROTATIONS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA, position=p).mark_image()
-            self.assertEqual(str(error.exception), 'The position values must be numeric.')
-
-    def test_rotation_error(self):
-        """Mark image rotation error."""
-        for r in NOT_3AXIS_POSITIONS_ROTATIONS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA, rotation=r).mark_image()
-            self.assertEqual(str(error.exception), f'The rotation: {r} is not correct. Must be "x y z".')
-
-        for r in NOT_NUMERIC_POSITIONS_ROTATIONS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA, rotation=r).mark_image()
-            self.assertEqual(str(error.exception), 'The rotation values must be numeric.')
-
-    def test_width_error(self):
-        """Mark image width error."""
-        for w in NOT_GREATER_THAN_0_MARK_IMAGE_WIDTHS_HEIGHTS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA).mark_image(width=w)
-            self.assertEqual(str(error.exception), 'The width must be greater than 0.')
-
-    def test_height_error(self):
-        """Mark image height error."""
-        for h in NOT_GREATER_THAN_0_MARK_IMAGE_WIDTHS_HEIGHTS:
-            with self.assertRaises(ValueError) as error:
-                aframexr.Chart(DATA).mark_image(height=h)
-            self.assertEqual(str(error.exception), 'The height must be greater than 0.')
