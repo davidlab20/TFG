b998b1306e06265e5104b7bb27d656d1e383b759
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-02-09 13:58:46 +0100

    Added tests for Octahedron creations

---
+++ tests/test_octahedron.py
@@ -0,0 +1,81 @@
+import aframexr
+import unittest
+
+from aframexr.utils.constants import ERROR_MESSAGES
+from .constants import *
+
+
+class TestOctahedronOK(unittest.TestCase):
+    """Tests for simple octahedron creation."""
+
+    def test_simple(self):
+        """Simple octahedron creation."""
+        octahedron = aframexr.Octahedron()
+        octahedron.to_html()
+
+    def test_from_dict(self):
+        """Octahedron creation using form_dict() method."""
+        octahedron = aframexr.Octahedron.from_dict(aframexr.Octahedron().to_dict())
+        octahedron.to_html()
+
+    def test_from_json(self):
+        """Octahedron creation using form_json() method."""
+        octahedron = aframexr.Octahedron.from_json(aframexr.Octahedron().to_json())
+        octahedron.to_html()
+
+    def test_color(self):
+        """Octahedron with specific color creation."""
+        for c in SIMPLE_ELEMENTS_COLORS:
+            octahedron = aframexr.Octahedron(color=c)
+            octahedron.to_html()
+
+    def test_position(self):
+        """Octahedron with specific position creation."""
+        for p in POSITIONS + POSITION_FORMATS:
+            octahedron = aframexr.Octahedron(position=p)
+            octahedron.to_html()
+
+    def test_radius(self):
+        """Octahedron with specific radius creation."""
+        for r in MARK_ARC_RADIUS:
+            octahedron = aframexr.Octahedron(radius=r)
+            octahedron.to_html()
+
+    def test_all_parameters(self):
+        """Octahedron creation with all parameters specified."""
+        for c, p, r in zip(SIMPLE_ELEMENTS_COLORS, POSITIONS, MARK_ARC_RADIUS):
+            octahedron = aframexr.Octahedron(color=c, position=p, radius=r)
+            octahedron.to_html()
+
+    def test_concatenation(self):
+        """Concatenation of simple octahedron."""
+        concatenated_chart = aframexr.Octahedron(position=CONCATENATION_POSITIONS[0])
+        for p in CONCATENATION_POSITIONS[1:]:
+            concatenated_chart += aframexr.Octahedron(position=p)
+        concatenated_chart.to_html()
+
+
+class TestOctahedronERROR(unittest.TestCase):
+    """Tests for octahedron creation error."""
+
+    def test_color_error(self):
+        """Octahedron with specific color creation error."""
+        not_an_string = ['not a color']
+        with self.assertRaises(TypeError) as error:
+            # noinspection PyTypeChecker
+            octahedron = aframexr.Octahedron(color=not_an_string)
+            octahedron.to_html()
+        self.assertEqual(
+            str(error.exception),
+            ERROR_MESSAGES['TYPE'].format(
+                param_name='specs.color', expected_type=str.__name__, current_type=type(not_an_string).__name__
+            )
+        )
+
+    def test_radius_error(self):
+        """Octahedron with specific radius creation error."""
+        for r in NOT_GREATER_THAN_0_MARK_ARC_RADIUS:
+            with self.assertRaises(ValueError) as error:
+                octahedron = aframexr.Octahedron(radius=r)
+                octahedron.to_html()
+            self.assertEqual(str(error.exception), ERROR_MESSAGES['POSITIVE_NUMBER'].format(param_name='specs.radius'))
