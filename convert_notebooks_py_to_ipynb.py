77976c2bf0e0e432e8fb5bfc574b2de848de3099
davidlab <d.diazg.2022@alumnos.urjc.es>
2026-02-15 09:14:24 +0100

    Added program to convert notebooks from .py to .ipynb

---
+++ convert_notebooks_py_to_ipynb.py
@@ -0,0 +1,26 @@
+"""Program to convert the .py notebooks into .ipynb for MyBinder."""
+
+import glob
+import os
+import subprocess
+
+NOTEBOOKS_DIR = 'docs/examples/notebooks'
+
+try:
+    # Change into docs/examples/notebooks/ directory
+    os.chdir(NOTEBOOKS_DIR)
+
+    # Convert all .py notebooks into .ipynb notebooks for MyBinder
+    py_files = glob.glob('*.py')
+    if py_files:
+        subprocess.run(['jupytext', '--to', 'notebook'] + py_files, check=True)
+        print('Converted notebooks into .ipynb')
+    else:
+        print('No .py notebooks found')
+
+except FileNotFoundError:
+    print(f'The directory {NOTEBOOKS_DIR} does not exist')
+except subprocess.CalledProcessError:
+        print('Error while running jupytext command. Make sure jupytext is installed and available')
+except Exception as e:
+        print(f"An unexpected error occurred: {e}")
