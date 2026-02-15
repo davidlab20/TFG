d7d3cdcc3ce45677018916ab9804988771d59e46
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-02-15 20:32:41 +0100

    Updated convert_notebooks_py_to_ipynb.py (as now we do not need to change directory)

--- docs/examples/notebooks/convert_notebooks_py_to_ipynb.py
+++ docs/examples/notebooks/convert_notebooks_py_to_ipynb.py
@@ -1,15 +1,11 @@
 """Program to convert the .py notebooks into .ipynb for MyBinder."""
 
 import glob
-import os
 import subprocess
 
 NOTEBOOKS_DIR = ''
 
 try:
-    # Change into docs/examples/notebooks/ directory
-    os.chdir(NOTEBOOKS_DIR)
-
     # Convert all .py notebooks into .ipynb notebooks for MyBinder
     py_files = glob.glob('*.py')
     if py_files:
@@ -18,8 +14,6 @@ try:
     else:
         print('No .py notebooks found')
 
-except FileNotFoundError:
-    print(f'The directory {NOTEBOOKS_DIR} does not exist')
 except subprocess.CalledProcessError:
         print('Error while running jupytext command. Make sure jupytext is installed and available')
 except Exception as e:
