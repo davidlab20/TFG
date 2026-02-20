2f218463bd22b7d1ea7fc751943fe9fc3cd9302f
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-02-20 13:44:08 +0100

    Removed unused if

--- aframexr/utils/axis_creator.py
+++ aframexr/utils/axis_creator.py
@@ -60,20 +60,18 @@ class AxisCreator:
     """Axis creator class."""
 
     @staticmethod
-    def create_axis_html(start: str | None, end: str | None) -> str:
+    def create_axis_html(start: str, end: str) -> str:
         """
         Create a line for the axis and returns its HTML.
 
         Parameters
         ----------
-        start : str | None
-            The base position of each axis. If None, no axis is displayed.
-        end : str | None
-            The end position of the axis. If None, no axis is displayed.
+        start : str
+            The base position of each axis.
+        end : str
+            The end position of the axis.
         """
-        if start and end:
-            return f'<a-entity line="start: {start}; end: {end}; color: black"></a-entity>'
-        return ''
+        return f'<a-entity line="start: {start}; end: {end}; color: black"></a-entity>'
 
     @staticmethod
     def create_label_html(pos: str, rotation: str, value: str, align: Literal['left', 'center', 'right']) -> str:
