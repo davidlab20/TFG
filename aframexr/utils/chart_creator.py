c0a02c52b41cc77b893302d2f87bbf2f812e57a1
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-03-09 11:11:29 +0100

    Added axes' size round

--- aframexr/utils/chart_creator.py
+++ aframexr/utils/chart_creator.py
@@ -200,7 +200,7 @@ class XYZAxisChannelChartCreator(ChartCreator):
             if _translate_dtype_into_encoding(axis_data.dtype) == 'quantitative':
                 return default_axis_size  # User did not define bars' size or axis is quantitative
 
-            return elem_size * axis_data.n_unique()
+            return round(elem_size * axis_data.n_unique(), PRECISION_DECIMALS)
 
         # X-axis
         if self._chart_width is None:  # User did not define chart width
