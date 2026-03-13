cc66b86b99d697439684661e2eba7ee4c6980b17
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-03-13 14:19:16 +0100

    Added demo for an AR scene

---
+++ docs/demos/ar_demo.py
@@ -0,0 +1,18 @@
+import aframexr
+
+DATA = aframexr.UrlData('https://cdn.jsdelivr.net/gh/davidlab20/TFG@main/docs/static/data/data.json')
+BASE = aframexr.Chart(DATA, depth=1)
+
+param = aframexr.selection_point('param', ['motor'])
+charts = (
+    # Base bar chart
+    BASE.mark_bar().encode(x='motor', y='sum(sales)').properties(title='Sales by motor', position='0 2 0').add_params(param)
+
+    +
+
+    # Dynamic pie chart
+    BASE.mark_arc().encode(color='model', theta='sales').properties(position='4 2 0').transform_filter(param).movable()
+)
+
+# Save scene
+charts.save('ar-demo.html')
