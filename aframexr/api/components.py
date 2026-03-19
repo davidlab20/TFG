2b490930d15dab2b0182421ce77336e3ca1015ab
davidlab20 <gdavid.diazgarcia@gmail.com>
2026-03-19 10:36:36 +0100

    Increased time and memory performance

--- aframexr/api/components.py
+++ aframexr/api/components.py
@@ -247,8 +247,8 @@ class Chart(TopLevelMixin):
         elif isinstance(data, UrlData):
             self._specifications['data'] = {'url': data.url}
         elif pd is not None and isinstance(data, pd.DataFrame):
-            # More efficient than using pd.to_dict(orient='records')
-            self._specifications['data'] = {col: data[col].tolist() for col in data.columns}
+            # More efficient than using pd.to_dict(orient='records') and data[col].tolist() only
+            self._specifications['data'] = {col: data[col].to_numpy().tolist() for col in data.columns}
         else:  # pragma: no cover (AframeXRValidator.validate_type() should have validate data type)
             raise RuntimeError('Unreachable code: AframeXRValidator.validate_type() should have validate data type')
 
