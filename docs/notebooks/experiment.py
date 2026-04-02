3db93ecd3f098c4b9e4bf3ad73e33b3dd3f6cb70
davidlab <d.diazg.2022@alumnos.urjc.es>
2026-04-02 09:02:07 +0200

    Updated experiment notebook

--- docs/notebooks/experiment.py
+++ docs/notebooks/experiment.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.21.1"
+__generated_with = "0.22.0"
 app = marimo.App(width="medium")
 
 
@@ -121,7 +121,6 @@ def _(mo):
 @app.cell
 def _():
     # Write your code here
-
     return
 
 
@@ -155,7 +154,6 @@ def _(mo):
 @app.cell
 def _():
     # Write your code here
-
     return
 
 
@@ -197,7 +195,6 @@ def _(mo):
 @app.cell
 def _():
     # Write your code here
-
     return
 
 
@@ -225,7 +222,7 @@ def _(mo):
     - x: model
     - y: sales
     - color: motor
-    - size: doors (take into account that doors is a **quantitative** value)
+    - size: doors (take into account that doors is a **quantitative** value, but nominal is needed)
     """)
     return
 
@@ -233,7 +230,6 @@ def _(mo):
 @app.cell
 def _():
     # Write your code here
-
     return
 
 
@@ -254,19 +250,145 @@ def _(sol_task4):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    ### <u>Task 5 — 3D Filtered bar chart</u>
+    ### <u>Task 5 — 3D Filtered Bar chart</u>
 
-    Create a 3D filtered Bar Chart:
+    Create a 3D filtered Bar chart:
 
     - x: model
     - y: sales
-    - filter: datum.motor == "diesel"
+    - filter transformation: datum.motor == "diesel"
+    """)
+    return
+
+
+@app.cell
+def _():
+    # Write your code here
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    You should see something like this:
     """)
     return
 
 
 @app.cell(hide_code=True)
-def _(aframexr, df_pd):
+def _(sol_task5):
+    sol_task5
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ## High-level tasks
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo, task6_url):
+    mo.md(f"""
+    ### <u>Task 6 — 3D Line chart with several lines</u>
+
+    Create a 3D Line chart:
+
+    - data's URL: {task6_url} (TIP: look at the objects that Chart() receives)
+    - using point markers
+    - x: year (take into account that year is a **quantitative** value)
+    - y: sales
+    - color: model
+    """)
+    return
+
+
+@app.cell
+def _():
+    # Write your code here
+    task6_url = "https://cdn.jsdelivr.net/gh/davidlab20/TFG@main/docs/static/data/model_year_sales.json"
+    return (task6_url,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    You should see something like this:
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(sol_task6):
+    sol_task6
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ## Complex tasks
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    ### <u>Task 7 — 3D Dynamic filtered chart</u>
+
+    Create a parameter with point selection (filtered when click):
+
+    - name: "param" (you can use another)
+    - fields: motor
+
+    Create the 3D main chart (Bar chart):
+
+    - color of all the bars: green (no the same as color encoding)
+    - x: motor
+    - y: sum(sales)
+    - add the parameter created before
+
+    Create the 3D dynamic filtered chart (Pie chart):
+
+    - color: model
+    - theta: sales
+    - filter transformation: the parameter created before
+
+    Add both charts in the same scene
+
+    **TIPS:**
+
+    - charts may have different positions so they can be watched correctly
+    - click on the main chart to see the dynamic filtering
+    """)
+    return
+
+
+@app.cell
+def _():
+    # Write your code here
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    You should see something like this:
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(sol_task7):
+    sol_task7
+    return
+
+
+@app.cell(hide_code=True)
+def _(aframexr, df_pd, task6_url):
     # ===== Solutions =====
 
     global_position = '0 2 -5'  # Position for every chart (for good visualization)
@@ -298,7 +420,49 @@ def _(aframexr, df_pd):
         color="motor",
         size="doors:Q"
     )
-    return sol_task1, sol_task2, sol_task3, sol_task4
+
+    # Task 5
+    sol_task5 = base.mark_bar().encode(
+        x="model",
+        y="sales"
+    ).transform_filter("datum.motor == 'diesel'")
+
+    # Task 6
+    sol_task6 = base.mark_line(point=True).encode(
+        x="year:N",
+        y="sales",
+        color="model"
+    ).properties(data=aframexr.UrlData(task6_url))
+
+    # Task 7
+    param_task7 = aframexr.selection_point(
+        name="param",
+        fields=["motor"]
+    )
+
+    main_task7 = base.mark_bar(color="green").encode(
+        x="motor",
+        y="sum(sales)"
+    ).add_params(param_task7).properties(
+        position="-3 2 -6"
+    )
+    filtered_task7 = base.mark_arc().encode(
+        color="model",
+        theta="sales"
+    ).transform_filter(param_task7).properties(
+        position="3 2 -6"
+    )
+
+    sol_task7 = main_task7 + filtered_task7
+    return (
+        sol_task1,
+        sol_task2,
+        sol_task3,
+        sol_task4,
+        sol_task5,
+        sol_task6,
+        sol_task7,
+    )
 
 
 if __name__ == "__main__":
